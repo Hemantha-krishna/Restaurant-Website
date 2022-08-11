d20931b8fd45ebe9f7f5b1642b57c93afe10d143
Hemantha-krishna <hemanthakrishna_challa@srmap.edu.in>
2022-08-11 12:59:32 +0530

    comments

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 //login
+
 document.getElementById("login").addEventListener("click", ()=> {
     document.querySelector(".popup").style.display = "flex";
 });
 document.querySelector(".close").addEventListener("click", ()=> {
     document.querySelector(".popup").style.display = "none";
 });
+
 //signup
+
 document.getElementById("signup").addEventListener("click", ()=> {
     document.querySelector(".popup2").style.display = "flex";
 });
 document.querySelector(".close2").addEventListener("click", ()=> {
     document.querySelector(".popup2").style.display = "none";
 });
-// Previous Orders
-document.getElementById("prev_order").addEventListener("click",()=>
-{
-    document.querySelector(".popup3").style.display="flex";
-});
-document.querySelector(".close3").addEventListener("click", ()=> {
-    document.querySelector(".popup3").style.display = "none";
-});
+
 
 
 
