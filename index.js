7ada52d8b9ddcaa4c1a9932ff6607e2c1c95d4f8
Hemantha-krishna <hemanthakrishna_challa@srmap.edu.in>
2022-08-11 13:02:56 +0530

    changes

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 //login
 
-document.getElementById("login").addEventListener("click", ()=> {
-    document.querySelector(".popup").style.display = "flex";
+document.getElementById("login").addEventListener("click", () => {
+  document.querySelector(".popup").style.display = "flex";
 });
-document.querySelector(".close").addEventListener("click", ()=> {
-    document.querySelector(".popup").style.display = "none";
+document.querySelector(".close").addEventListener("click", () => {
+  document.querySelector(".popup").style.display = "none";
 });
 
 //signup
 
-document.getElementById("signup").addEventListener("click", ()=> {
-    document.querySelector(".popup2").style.display = "flex";
+document.getElementById("signup").addEventListener("click", () => {
+  document.querySelector(".popup2").style.display = "flex";
 });
-document.querySelector(".close2").addEventListener("click", ()=> {
-    document.querySelector(".popup2").style.display = "none";
+document.querySelector(".close2").addEventListener("click", () => {
+  document.querySelector(".popup2").style.display = "none";
 });
-
-
-
-
